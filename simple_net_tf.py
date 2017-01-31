173df7bedf30ed91a6a6e71af62a3842f0eaa08b
satojkovic <satojkovic@gmail.com>
2017-02-01 08:20:21 +0900

    Load an input data

--- simple_net_tf.py
+++ simple_net_tf.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
 
 def main():
-    pass
+    # load an input data
+    digits = load_digits()
+    X_train, X_test, y_train, y_test = train_test_split(digits.data,
+                                                        digits.target)
+    print('Train data: {}'.format(X_train.shape))
+    print('Test data: {}'.format(X_test.shape))
 
 
 if __name__ == '__main__':
